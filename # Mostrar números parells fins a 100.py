174aa22a63652927f5ead6535491e28d98b24204
Ivanleon123 <imunyoz20974@iesjoanramis.org>
2024-11-29 12:38:11 +0100

    Add files via upload

---
+++ # Mostrar números parells fins a 100.py
@@ -0,0 +1,11 @@
+# Mostrar números parells fins a 100
+print("Números parells:")
+for i in range(2, 101, 2):
+    print(i, end=", ")
+print("\b\b ")  # Per eliminar la última coma i espai
+
+# Mostrar números senars fins a 100
+print("\nNúmeros senars:")
+for i in range(1, 100, 2):
+    print(i, end=", ")
+print("\b\b ")  # Per eliminar la última coma i espai
